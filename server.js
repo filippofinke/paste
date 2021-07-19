d0178497b9d1dafc47a48fdd5276c41ab4157e20
Filippo Finke <filippofinke@protonmail.ch>
2021-07-19 10:21:46 +0200

    🎉 Initial commit

---
+++ server.js
@@ -0,0 +1,87 @@
+require("dotenv").config();
+const StormDB = require("stormdb");
+const express = require("express");
+const path = require("path");
+const bcrypt = require("bcrypt");
+const uuidv4 = require("uuid").v4;
+
+process.env.PORT = process.env.PORT || 3000;
+process.env.SALT_ROUNDS = process.env.SALT_ROUNDS || 10;
+const app = express();
+
+const engine = new StormDB.localFileEngine(path.join(__dirname, "db.stormdb"));
+const db = new StormDB(engine);
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "build")));
+
+app.get("/paste/:id", (req, res) => {
+	let id = req.params.id;
+
+	let paste = db.get(id).value();
+	if (paste) {
+		return res.json({ text: paste.text });
+	} else {
+		return res.sendStatus(404);
+	}
+});
+
+app.post("/paste", (req, res) => {
+	let master = req.headers["x-paste-master"];
+
+	if (master) {
+		let id = uuidv4();
+		while (typeof db.get(id).value() != "undefined") {
+			id = uuidv4();
+		}
+		bcrypt.hash(master, Number(process.env.SALT_ROUNDS), (err, hash) => {
+			if (err) {
+				return res.sendStatus(500);
+			} else {
+				db.set(id, {
+					master: hash,
+					text: "",
+				});
+
+				db.save();
+
+				return res.json({ id: id });
+			}
+		});
+	} else {
+		return res.sendStatus(400);
+	}
+});
+
+app.put("/paste/:id", (req, res) => {
+	let id = req.params.id;
+	let master = req.headers["x-paste-master"];
+	let text = req.body.text;
+	if (master && text) {
+		let paste = db.get(id).value();
+
+		if (paste) {
+			bcrypt.compare(master, paste.master, (err, result) => {
+				if (err) {
+					return res.sendStatus(500);
+				}
+
+				if (result) {
+					db.get(id).get("text").set(text);
+					db.save();
+					return res.sendStatus(200);
+				} else {
+					return res.sendStatus(401);
+				}
+			});
+		} else {
+			return res.sendStatus(404);
+		}
+	} else {
+		return res.sendStatus(400);
+	}
+});
+
+app.listen(process.env.PORT, () => {
+	console.log("server started on port " + process.env.PORT);
+});
--- src/App.js
+++ src/App.js
@@ -1,25 +1,116 @@
-import logo from './logo.svg';
-import './App.css';
+import "react-quill/dist/quill.snow.css";
+import "./App.css";
+import ReactQuill from "react-quill";
+import { useEffect, useState } from "react";
+import aes256 from "aes256";
+import generator from "generate-password";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	const [masterPassword, setMasterPassword] = useState("");
+	const [id, setId] = useState("");
+	const [password, setPassword] = useState("");
+	const [text, setText] = useState("");
+	const [readOnly, setReadOnly] = useState(false);
+
+	let modules = [
+		[{ font: [] }, { size: [] }],
+		[{ align: [] }, "direction"],
+		["bold", "italic", "underline", "strike"],
+		[{ color: [] }, { background: [] }],
+		[{ script: "super" }, { script: "sub" }],
+		["blockquote", "code-block"],
+		[
+			{ list: "ordered" },
+			{ list: "bullet" },
+			{ indent: "-1" },
+			{ indent: "+1" },
+		],
+		["link"],
+		["clean"],
+	];
+
+	const handleTextChange = (text) => {
+		let encrypted = aes256.encrypt(password, text);
+		setText(text);
+		fetch("/paste/" + id, {
+			method: "PUT",
+			headers: {
+				"Content-Type": "application/json",
+				"X-Paste-Master": masterPassword,
+			},
+			body: JSON.stringify({ text: encrypted }),
+		});
+	};
+
+	const copyLinkToClipboard = () => {
+		let url =
+			window.location.protocol +
+			"//" +
+			window.location.host +
+			"/" +
+			id +
+			"#" +
+			password;
+		navigator.clipboard
+			.writeText(url)
+			.then(() => {})
+			.catch((err) => {});
+	};
+
+	useEffect(() => {
+		let id = window.location.pathname.substr(1);
+		let pass = window.location.hash.substr(1);
+		if (id && pass) {
+			fetch("/paste/" + id, {
+				method: "GET",
+				headers: {
+					"Content-Type": "application/json",
+				},
+			})
+				.then((response) => response.json())
+				.then((json) => {
+					let text = "";
+					if (json.text) text = aes256.decrypt(pass, json.text);
+					setText(text);
+					setReadOnly(true);
+				});
+		}
+
+		setPassword(generator.generate({ length: 24, numbers: true }));
+		let master = generator.generate({ length: 64, numbers: true });
+		setMasterPassword(master);
+
+		fetch("/paste", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+				"X-Paste-Master": master,
+			},
+		})
+			.then((response) => response.json())
+			.then((json) => {
+				setId(json.id);
+			});
+		// eslint-disable-next-line
+	}, []);
+
+	return (
+		<div className="app">
+			<ReactQuill
+				className="editor"
+				theme="snow"
+				modules={{ toolbar: readOnly ? false : modules }}
+				value={text}
+				onChange={handleTextChange}
+				readOnly={readOnly}
+			/>
+			{!readOnly && (
+				<div className="link">
+					<button onClick={copyLinkToClipboard}>Click to copy the link</button>
+				</div>
+			)}
+		</div>
+	);
+};
 
 export default App;
